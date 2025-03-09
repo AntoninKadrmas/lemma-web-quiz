699eb31800e306c6238bc53ef4995014d9ee69ca
Antonín Kadrmas <xkadrmas@fi.muni.cz>
2025-03-09 13:50:18 +0100

    working lint

--- app/api/question/[id]/route.ts
+++ app/api/question/[id]/route.ts
@@ -1,18 +1,13 @@
 import { NextResponse } from "next/server";
 import quizData from "../../data/quiz";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET({ params }: { params: { id: string } }) {
   const { id } = params;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { correctId, ...question } = quizData.find((q) => q.id === id)!; // opravit pak
-
   if (!question) {
     return NextResponse.json({ error: "Quiz not found" }, { status: 404 });
   }
 
   return NextResponse.json(question);
 }
-
-
