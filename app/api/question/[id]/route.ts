1997a111c40e6e8575cb2754f8d4c575ea66eaee
Antonín Kadrmas <xkadrmas@fi.muni.cz>
2025-03-09 13:55:29 +0100

    quick fix

--- app/api/question/[id]/route.ts
+++ app/api/question/[id]/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import quizData from "../../data/quiz";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   const { id } = params;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { correctId, ...question } = quizData.find((q) => q.id === id)!; // opravit pak
